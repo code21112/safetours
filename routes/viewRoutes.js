4ad10906dbd1b7e5da78f15190f783133fe0b745
code21112 <code21112@protonmail.com>
2020-10-21 12:17:03 -0400

    Webhook for Stripe

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,12 +6,12 @@ const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, bookingController.createBookingCheckout, viewController.getOverview);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewController.getSignupForm);
 router.get('/me', authController.protect, viewController.getAccount);
-router.get('/my-bookings', bookingController.createBookingCheckout, authController.protect, viewController.getMyBookings);
+router.get('/my-bookings', authController.protect, viewController.getMyBookings);
 
 // router.post('/submit-user-data', authController.protect, viewController.updateUserData);
 // router.get('/updateMe', authController.protect, userController.updateMe);
