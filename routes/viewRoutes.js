b957bdc4bcb595c3405f3ea4bc14979434d04ade
code21112 <code21112@protonmail.com>
2020-10-21 09:36:22 -0400

    Changing viewRoutes adding middleware

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -11,7 +11,7 @@ router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewController.getSignupForm);
 router.get('/me', authController.protect, viewController.getAccount);
-router.get('/my-bookings', authController.protect, viewController.getMyBookings);
+router.get('/my-bookings', bookingController.createBookingCheckout, authController.protect, viewController.getMyBookings);
 
 // router.post('/submit-user-data', authController.protect, viewController.updateUserData);
 // router.get('/updateMe', authController.protect, userController.updateMe);
