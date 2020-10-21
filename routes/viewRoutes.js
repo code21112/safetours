69c8c0b4f5b7c6a7903f021b21c01b18d00e552d
code21112 <code21112@protonmail.com>
2020-10-21 15:28:43 -0400

    Correcting error in viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,7 +6,7 @@ const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
-router.use(viewController.alert);
+router.use(viewController.alerts);
 
 router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
